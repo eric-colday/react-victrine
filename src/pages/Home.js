d1073f59ce8f7d01174ce3e4f4642f0d318ee7e5
Eric Noubissi <noubissi10@yahoo.fr>
2022-02-21 10:55:03 +0100

    Ajout DynamicText et ButtonsBottom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import DynamicText from '../components/DynamicText';
 import Navigation from '../components/Navigation';
 import SocialNetwork from '../components/SocialNetwork';
 
 const Home = () => {
     return (
         <div>
-            <Navigation />
-            <SocialNetwork />
+            <div className="home">
+                <Navigation />
+                <SocialNetwork />
+                <div className="home-main">
+                    <div className="main-content">
+                        <h1>NEC AGENCY</h1>
+                        <h2><DynamicText /></h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
